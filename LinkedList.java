5e9725f124690e14156f19f677556cf10fbc43ff
Joshua Stupplebeen <jpstup@gmail.com>
2018-08-22 17:02:12 -0600

    init

---
+++ LinkedList.java
@@ -0,0 +1,13 @@
+// To Run
+// javac -d ./ *.java
+// java -cp ${classpath} *.class
+
+package wolverine;
+import wolverine.Node;
+
+public class LinkedList {
+  public static void main(String args[]) {
+    Node n = new Node("Node item");
+    System.out.println(n.item);
+  }
+}
